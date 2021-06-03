643222469543719f11d1cf30fa58964816cc93c3
riadh26 <riadh.by.laabidi@gmail.com>
2021-06-03 05:10:35 +0100

    Implement stage 2/5: Multiple recipes

--- src/main/java/com/example/recipe/Api.java
+++ src/main/java/com/example/recipe/Api.java
@@ -1,20 +1,27 @@
 package com.example.recipe;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class Api {
+    private Map<Long, Recipe> recipes = new HashMap<>();
 
-    private Recipe recipe;
-
-    @PostMapping(value = "/recipe", consumes = "application/json")
-    public void addRecipe(@RequestBody Recipe recipe) {
-        this.recipe = recipe;
+    @PostMapping(value = "/recipe/new", consumes = "application/json")
+    public ResponseEntity<Map<String, Long>> newRecipe(@RequestBody Recipe recipe) {
+        recipes.put(recipe.getId(), recipe);
+        return ResponseEntity.ok(Map.of("id", recipe.getId()));
     }
 
-    @GetMapping("/recipe")
-    public Recipe getRecipe() {
-        return recipe;
+    @GetMapping("/recipe/{id}")
+    public ResponseEntity<Recipe> getRecipe(@PathVariable long id) {
+        if (recipes.containsKey(id)) {
+            return ResponseEntity.ok(recipes.get(id));
+        }
+        return ResponseEntity.notFound().build();
     }
 }
